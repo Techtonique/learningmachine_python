1cb9c2c7e951241ab9f8a0b6acc5f67c1fb13b4d
Thierry Moudiki <thierry.moudiki@gmail.com>
2024-01-21 20:10:14 +0100

    install R

--- setup.py
+++ setup.py
@@ -14,6 +14,13 @@ else:
 
 subprocess.run(['pip', 'install', 'rpy2>=3.4.5'])
 
+from rpy2.robjects import r
+
+try: 
+    r("utils.install.packages('learningmachine', repos = c('https://techtonique.r-universe.dev', 'https://cran.r-project.org'))")
+except Exception as e:
+    subprocess.run(['Rscript', '-e', "utils.install.packages('learningmachine', repos = c('https://techtonique.r-universe.dev', 'https://cran.r-project.org'))"])
+
 """The setup script."""
 here = path.abspath(path.dirname(__file__))
 
