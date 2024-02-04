85be263c8dd5cfa3be85dbb8aafa369b5b2d9ac8
Thierry Moudiki <thierry.moudiki@gmail.com>
2024-02-04 16:53:40 +0100

    update setup.py Pt.5

--- setup.py
+++ setup.py
@@ -102,15 +102,15 @@ else:
 # 4 - Package setup -----------------------------------------------
     
 """The setup script."""
-try: 
+try:
+    install_requires = ['numpy', 'pandas', 'rpy2>=3.4.5', 'scikit-learn', 'scipy']     
+except Exception as e:
     here = path.abspath(path.dirname(__file__))
     # get the dependencies and installs
     with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
         all_reqs = f.read().split("\n")
         install_requires = [x.strip() for x in all_reqs if "git+" not in x]
-except Exception as e:
-    print(e)
-    install_requires = ['numpy', 'pandas', 'rpy2>=3.4.5', 'scikit-learn', 'scipy']
+    
 
 dependency_links = [
     x.strip().replace("git+", "") for x in all_reqs if x.startswith("git+")
