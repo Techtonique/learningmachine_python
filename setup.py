3c2b88ea4bc1c3012aa06adbae16365dcaf153e7
Thierry Moudiki <thierry.moudiki@gmail.com>
2024-08-29 16:23:55 +0200

    align with R version

--- setup.py
+++ setup.py
@@ -169,6 +169,6 @@ setup(
     packages=find_packages(include=["learningmachine", "learningmachine.*"]),
     test_suite="tests",
     url="https://github.com/Techtonique/learningmachine_python",
-    version="2.3.1",
+    version="2.4.0",
     zip_safe=False,
 )
